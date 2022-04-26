3c4a68ada5faff68cb7580a403c25cde452f81f9
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-04-26 20:23:21 +0000

    python function

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -6,6 +6,8 @@
 #
 # @return The output of this action, which must be a JSON object.
 #
+
+
 #
 from cloudant.client import Cloudant
 from cloudant.error import CloudantException
@@ -30,3 +32,16 @@ def main(dict):
         return {"error": err}
 
     return {"dbs": client.all_dbs()}
+    
+
+dict={
+  "IAM_API_KEY": "6COGnWPRUVwZ0QsBCYFGAvA53MjNAtgbkLd1XHicSglm",
+  "host": "fbcd12ae-b980-4832-8e9b-57526f7a52ad-bluemix.cloudantnosqldb.appdomain.cloud",
+  "iam_apikey_description": "Auto-generated for key crn:v1:bluemix:public:cloudantnosqldb:au-syd:a/2d8bc3f0f518465d92fc2c0cd0008ded:8d112daf-62bb-4a52-b963-e0c6cc19be72:resource-key:daa33066-74f5-4b7b-abbf-3b66d7c9a6de",
+  "iam_apikey_name": "for-guestbook",
+  "iam_role_crn": "crn:v1:bluemix:public:iam::::serviceRole:Manager",
+  "iam_serviceid_crn": "crn:v1:bluemix:public:iam-identity::a/2d8bc3f0f518465d92fc2c0cd0008ded::serviceid:ServiceId-6448d0a4-bbcb-4749-9ccb-93577e618c2f",
+  "URL": "https://fbcd12ae-b980-4832-8e9b-57526f7a52ad-bluemix.cloudantnosqldb.appdomain.cloud",
+  "COUCH_USERNAME": "fbcd12ae-b980-4832-8e9b-57526f7a52ad-bluemix"
+}
+print(main(dict))
