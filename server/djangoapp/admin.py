983495b319a116193e42d49dc3b17905dc99790f
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-05-07 14:56:31 +0000

    admin, register,models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,23 @@
 from django.contrib import admin
 # from .models import related models
+from .models import CarMake,CarModel
+
+admin.site.register(CarMake,CarModel)
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.ModelAdmin):
+    pass
+
+class CarModelAdmin(admin.ModelAdmin):
+    pass
 
 # CarModelAdmin class
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin:
+    inlines=[CarModelInline]
 
 # Register models here
