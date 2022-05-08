8aded975e45f1c1bbc6d74c1381100d89a902843
syedmubashirahmed <syedmubashirahmed@gmail.com>
2022-05-08 09:42:57 +0000

    inline

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,14 +2,15 @@ from django.contrib import admin
 # from .models import related models
 from .models import CarMake,CarModel
 
-admin.site.register(CarMake,CarModel)
+
 
 
 # Register your models here.
 
 # CarModelInline class
 class CarModelInline(admin.ModelAdmin):
-    pass
+    model=CarModel
+    extra=0
 
 class CarModelAdmin(admin.ModelAdmin):
     pass
@@ -21,3 +22,4 @@ class CarMakeAdmin:
     inlines=[CarModelInline]
 
 # Register models here
+admin.site.register(CarMake,CarModel)
